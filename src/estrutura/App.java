2df399939230073247937a3efe7a45c196fc442f
yujiichikawa <thiagogelinski20@gmail.com>
2022-11-25 10:42:02 -0300

    Criando os metodos listarPilha e adicionarElemento

--- src/estrutura/App.java
+++ src/estrutura/App.java
@@ -1,8 +1,22 @@
 package estrutura;
 
+import estrutura.model.Pilha;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		Pilha pilha = new Pilha(5);
+		
+		System.out.println("###########Pilha############");
+		
+		System.out.printf("Está vazia: %b\n",pilha.estaVazio());
+		System.out.printf("Está cheio: %b\n", pilha.estaCheio());
+		System.out.printf("Tamanho: %d\n", pilha.getTamanho());
+		
+		System.out.println("-----------Lista-----------");
+		
+		pilha.listarPilha();
 		
 		
 	}
--- src/estrutura/model/Pilha.java
+++ src/estrutura/model/Pilha.java
@@ -11,7 +11,30 @@ public class Pilha {
 		qtdElementos = 0;
 	}
 	
+	public boolean estaVazio() {
+		return (vetor[0] == null);
+	}
 	
+	public boolean estaCheio() {
+		return (vetor.length == qtdElementos);
+	}
+	
+	public void inserirElemento(String valor) {
+		for(int i=0;i<vetor.length;i++) {
+			if(vetor[i] == null) {
+				vetor[i] = valor;
+				break;
+			}
+		}
+	}
+	
+	public void listarPilha() {
+		for(int i=0;i<vetor.length;i++) {
+			if(vetor[i] != null) {
+				System.out.println("- "+vetor[i]);
+			}
+		}
+	}
 	
 	public int getTamanho() {
 		return tamanho;
